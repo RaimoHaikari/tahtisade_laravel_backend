e6c48e52171f1a0a2bdbc8e84e58cff173dc1cb1
Raimo Haikari <raimo.haikari@gmail.com>
2022-11-15 22:04:27 +0200

    Kuvien kanssa painiskelua

--- resources/js/frontEnd/pages/LandingPage/index.js
+++ resources/js/frontEnd/pages/LandingPage/index.js
@@ -1,9 +1,6 @@
 //import Jambo from "../../components/Logos/Jambo";
 //import Kansi from "../../../../images/tahtisade-kansi-leikattu.png";
 
-/*
-<img src="/images/slider-girl.png" />
-*/
 const LandingPage = () => {
 
     return (
@@ -15,13 +12,10 @@ const LandingPage = () => {
                     <div className="flow">
                         <h1 className="fs-primary-heading fw-bold">Tähtisadetta</h1>
                         <p className="fw-bold">Katsaus elokuvien ensi-ilta-arvosteluihin</p>
-                        <img src="./images/tahtisade-kansi-leikattu.png" alt="Kriitikko työnsä äärellä"/>
-                        <img src="/images/tahtisade-kansi-leikattu.png" alt="Kriitikko työnsä äärellä"/>
-                        <img src="public/images/tahtisade-kansi-leikattu.png" alt="Kriitikko työnsä äärellä"/>
                     </div>
 
                     <div>
-                        <img src="./public/images/tahtisade-kansi-leikattu.png" alt="Kriitikko työnsä äärellä"/>
+                        <img src="./images/tahtisade-kansi-leikattu.png" alt="Kriitikko työnsä äärellä"/>
                     </div>
                 </div>
             </div>
